d5ef4baf257d6a16b354f4a80864e143d3fff422
Max Zhao <max.zhao@princeton.edu>
2025-11-10 21:30:33 -0600

    Some fixes for preprocessing, add documentation

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -7,5 +7,7 @@ def test_whitening_matrices():
     data = np.random.rand(50, 5)
 
     W, W_inv, mu = whitening_matrices(data)
+    transformed = (data - mu) @ W.T
 
-    assert np.cov(W @ (data - mu).T) == pytest.approx(np.identity(5), abs=1e-4)
+    assert np.mean(transformed, axis=0) == pytest.approx(np.zeros(5), abs=1e-4)
+    assert np.cov(transformed, rowvar=False) == pytest.approx(np.identity(5), abs=1e-4)
