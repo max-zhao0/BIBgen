400c244bf0afad0bd352066a1983b19bbc5750cf
Max Zhao <max.zhao@princeton.edu>
2025-12-11 15:02:27 -0600

    Fixed evaluate return

--- src/BIBgen/training.py
+++ src/BIBgen/training.py
@@ -56,6 +56,4 @@ def evaluate(dataloader : DataLoader, model : torch.nn, loss_fn, device):
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
 
-    test_loss /= dataloader
-
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    return test_loss / dataloader.nsteps
